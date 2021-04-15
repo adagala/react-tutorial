22891154dbb07436f414e75a6a844ea558e4e920
Henry Adagala <adagalahenry@gmail.com>
2021-04-15 19:51:24 +0300

    Add new data to state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
 import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
+    characters: []
   }
 
   removeCharacter = (index) => {
@@ -33,12 +17,17 @@ class App extends Component {
     })
   }
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
   render() {
     const { characters } = this.state
 
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
